f2b9e70ba1774af3d19a257443ad334c261a23f2
Alex Anghel <alexisaky@gmail.com>
2024-01-12 17:57:42 +0200

    refactor

--- src/main/java/dev/alex_anghel/ListDiff.java
+++ src/main/java/dev/alex_anghel/ListDiff.java
@@ -7,18 +7,53 @@ import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @ApplicationScoped
 public class ListDiff {
     Logger LOG = LoggerFactory.getLogger(ListDiff.class);
+    Document doc;
 
-    public String extract(List<String> listOld) {
+    @PostConstruct
+    void getPage() {
+        try {
+            // fetching the target website
+            doc = Jsoup
+                    .connect("https://www.mygarage.ro/vanzari")
+                    .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36")
+                    .referrer("http://www.google.com")
+                    .get();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String compareFirstPost(String initialPost) {
+        String currentPost = getPost();
+        String response = addTimeToResponseMessage("no updates");
+
+        if (currentPost.equals(initialPost)) {
+            LOG.info(response);
+        } else {
+            response = addTimeToResponseMessage(currentPost);
+            LOG.info(response);
+        }
+        return response;
+    }
+
+    private static String addTimeToResponseMessage(String message) {
+        return LocalDateTime.now().withNano(0) + " " + message;
+    }
+
+    public String comparePosts(List<String> listOld) {
         List<String> listCurrent = getPosts();
+
         List<String> differences;
         String response = LocalDateTime.now().withNano(0) + " no updates";
 
@@ -29,33 +64,21 @@ public class ListDiff {
             differences.removeAll(listOld);
             response = LocalDateTime.now().withNano(0) + " " + differences;
             LOG.info(response);
-//            listOld = listCurrent;
         }
         return response;
     }
 
+    public String getPost() {
+        return doc.selectXpath("/html/body/div[2]/div/div/div/form[1]/table[3]/tbody[2]/tr[6]/td[3]/div[1]/a[1]").attr("href");
+    }
+
     public List<String> getPosts() {
-        List<String> listCurrent = new ArrayList<>();
+        List<String> currentPosts = new ArrayList<>();
 
-        // initializing the HTML Document page variable
-        Document doc;
-        try {
-            // fetching the target website
-            doc = Jsoup
-                    .connect("https://www.mygarage.ro/vanzari/")
-                    .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36")
-                    .referrer("http://www.google.com")
-                    .get();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        // retrieving the list of product HTML elements
-        // in the target page
         Elements nodes = doc.select("#threadbits_forum_35").get(0).selectXpath("//td[starts-with(@id,'td_threadtitle_')]");
         for (Element e : nodes) {
-            listCurrent.add(e.getElementsByAttributeStarting("href").last().attribute("href").getValue());
+            currentPosts.add(e.getElementsByAttributeStarting("href").last().attribute("href").getValue());
         }
-        return listCurrent;
+        return currentPosts;
     }
 }
--- src/main/java/dev/alex_anghel/MessageCalculator.java
+++ src/main/java/dev/alex_anghel/MessageCalculator.java
@@ -2,11 +2,15 @@ package dev.alex_anghel;
 
 import io.quarkus.scheduler.Scheduled;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 import java.util.List;
 
 @ApplicationScoped
+@Path("/")
 public class MessageCalculator {
 
     @Inject
@@ -14,14 +18,47 @@ public class MessageCalculator {
 
     @Inject
     ListDiff listDiff;
+    String initialPost;
+    List<String> initialPosts;
 
-    @Scheduled(every = "60s")
-    public void message() {
-        List<String> initialPosts = listDiff.getPosts();
-        String message = listDiff.extract(initialPosts);
+    @PostConstruct
+    void getInitialPost() {
+        initialPost = listDiff.getPost();
+        initialPosts = listDiff.getPosts();
+    }
+
+    @Scheduled(every = "10s")
+    public void run() {
+//        message();
+        messagePosts();
+    }
+
+//    public String message() {
+//        String message = listDiff.compareFirstPost(initialPost);
+//        if (!message.contains("no updates")) {
+//            bot.sendMessage(message);
+//        }
+//        return message;
+//    }
+
+    public String messagePosts() {
+        String message = listDiff.comparePosts(initialPosts);
         if (!message.contains("no updates")) {
             bot.sendMessage(message);
         }
+        return message;
+    }
+
+//    @GET()
+//    @Path("info")
+//    public String getPost() {
+//        return initialPost;
+//    }
+
+    @GET()
+    @Path("all")
+    public List<String> getAllPosts() {
+        return listDiff.getPosts();
     }
 
 }
